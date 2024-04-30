42a9ab6c8b04c22a2ba793e356b6a0cbc13dc51d
soufianeargane <soufianeargane800@gmail.com>
2024-04-30 12:59:06 +0200

    adding routes

--- movies/src/app/movie-details/movie-details.component.ts
+++ movies/src/app/movie-details/movie-details.component.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import {
+  IonHeader,
+  IonIcon,
+  IonToolbar,
+  IonButtons,
+  IonBackButton,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonContent,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-movie-details',
   templateUrl: './movie-details.component.html',
   styleUrls: ['./movie-details.component.scss'],
+  imports: [
+    IonContent,
+    IonCol,
+    IonRow,
+    IonGrid,
+    IonBackButton,
+    IonButtons,
+    IonToolbar,
+    IonIcon,
+    IonHeader,
+  ],
   standalone: true,
 })
-export class MovieDetailsComponent {
-  constructor() {}
+export class MovieDetailsComponent implements OnInit {
+  constructor(private route: ActivatedRoute) {}
+
+  movieId!: number;
+
+  ngOnInit() {
+    this.route.params.subscribe((params: Params) => {
+      console.log(`Movie details for movie with id: ${params['id']}`);
+      this.movieId = +params['id'];
+    });
+  }
 }
